d7d49dee4808ceae4593aeeac46f3488f1881b95
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2024-04-03 23:10:12 -0300

    Creates a method to filter only a specefic keyword

--- main.py
+++ main.py
@@ -1,40 +1,10 @@
-from moviepy.video.io.VideoFileClip import VideoFileClip
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-import speech_recognition as sr
-import os
+import io, json
+import whisper_integration as whis;
 
-def detect_and_cut(video_path, word_to_detect, output_path):
-    clip = VideoFileClip(video_path)
+result = whis.get_json_timestamped_result("gostaria")
+# Dump JSON data with indentation and ensure_ascii=False for non-ASCII characters
+json_data = json.dumps(result, indent=4, ensure_ascii=False)
 
-    # Extrai o áudio do vídeo
-    audio = clip.audio
-
-    # Salva o áudio em um arquivo temporário
-    temp_audio_path = "temp_audio.wav"
-    audio.write_audiofile(temp_audio_path)
-
-    # Usa a SpeechRecognition para transcrever o áudio
-    recognizer = sr.Recognizer()
-    with sr.AudioFile(temp_audio_path) as source:
-        audio_data = recognizer.record(source)
-
-    # Realiza a transcrição
-    try:
-        text = recognizer.recognize_google(audio_data, language='pt-BR')
-    except sr.UnknownValueError:
-        text = ""
-
-    # Busca a palavra no texto transcrito
-    if word_to_detect in text.lower():
-            print("Palavra encontrada!")
-            ffmpeg_extract_subclip(video_path, 0, 60, targetname=output_path)
-    else:
-            print("Palavra não encontrada!")
-    # Limpa o arquivo temporário
-    os.remove(temp_audio_path)
-
-# Exemplo de uso
-video_path = "C:\\Users\\kelvi-ribeiro\\Pictures\\Camera Roll\\WIN_20240221_22_27_32_Procom.mp4"
-word_to_detect = "gol"
-output_path = "C:\\Users\\kelvi-ribeiro\\Pictures\\Camera Roll\\gol.mp4"
-detect_and_cut(video_path, word_to_detect, output_path)
+# Save the JSON data to a file in a human-friendly pretty way
+with io.open('data.json', 'w', encoding='utf-8') as f:
+    f.write(json_data)
--- whisper_integration.py
+++ whisper_integration.py
@@ -1,19 +1,29 @@
 import whisper_timestamped as whisper
-import io, json
 
-def get_json_timestamped_result():
+def get_json_timestamped_result(keyword):
     audio = whisper.load_audio("Recording.m4a")
 
     model = whisper.load_model("tiny", device="cpu")
 
-    return whisper.transcribe(model, audio, language="pt")
+    result = whisper.transcribe(model, audio, language="pt")
+    return filter_json_by_keyword(result, keyword)
 
+def filter_json_by_keyword(json_data, keyword):
+    if not keyword:
+        return map_json_data(json_data)
+    
+    only_words_array = map_json_data(json_data)
+    filtered_results = []
+    for word in only_words_array: 
+        if keyword in word["text"]:
+            filtered_results.append(word)
 
+    return filtered_results
 
-result = get_json_timestamped_result()
-# Dump JSON data with indentation and ensure_ascii=False for non-ASCII characters
-json_data = json.dumps(result, indent=4, ensure_ascii=False)
+def map_json_data(json_data):
+    mapped_array = []
+    for segment in json_data["segments"]:
+        for word in segment["words"]:
+          mapped_array.append(word)
 
-# Save the JSON data to a file in a human-friendly pretty way
-with io.open('data.json', 'w', encoding='utf-8') as f:
-    f.write(json_data)
+    return mapped_array
