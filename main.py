972cd0351eafca1001c4ffdf8b92da287a93f16d
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2024-04-04 01:02:28 -0300

    Implementing first version of cut video method working

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
-import io, json
 import whisper_integration as whis;
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.editor import VideoClip, concatenate_videoclips
+ 
 
-result = whis.get_json_timestamped_result("gostaria")
-# Dump JSON data with indentation and ensure_ascii=False for non-ASCII characters
-json_data = json.dumps(result, indent=4, ensure_ascii=False)
+def cut_video(video_path, cuts, seconds_to_cut):
+    try:
+        # Load the video using MoviePy
+        video = VideoFileClip(video_path)
+    except Exception as e:
+        print(f"Error loading video: {e}")
+        return None  # Return None if video loading fails
 
-# Save the JSON data to a file in a human-friendly pretty way
-with io.open('data.json', 'w', encoding='utf-8') as f:
-    f.write(json_data)
+    # List to store the cut segments
+    cut_segments = []
+
+    # Loop through the start and end times to perform the cuts
+    for cut in cuts:
+        start_time = (cut.get("end") - seconds_to_cut)
+        end_time = cut.get("end")
+
+        # Check if start and end times are within video duration
+        if 0 <= start_time <= video.duration and 0 <= end_time <= video.duration:
+            # Cut the video within the specified interval
+            segment = video.subclip(start_time, end_time)
+            # Append the cut segment to the list
+            cut_segments.append(segment)
+        else:
+            print(f"Ignoring invalid cut: {cut}")
+
+    # Close the original video
+    #video.close() TODO COLOCAR PRA FECHAR NO MÉTODO QUE CHAMA
+
+    # Return the list of cut segments
+    return cut_segments
+
+
+filePath = "Video_Falado.mp4"
+result = whis.get_json_timestamped_result("manga", filePath)
+
+# Call the method to perform the cuts on the video
+cut_segments = cut_video(filePath, result, 3)
+
+# Combine the cut segments into a single video
+combined_video = concatenate_videoclips(cut_segments) # TRATAR VAZIO
+
+# Save the combined video to the root path
+output_path = 'final_video.mp4'
+combined_video.write_videofile(output_path)
--- whisper_integration.py
+++ whisper_integration.py
@@ -1,7 +1,8 @@
+import re
 import whisper_timestamped as whisper
 
-def get_json_timestamped_result(keyword):
-    audio = whisper.load_audio("Recording.m4a")
+def get_json_timestamped_result(keyword, filepath):
+    audio = whisper.load_audio(filepath)
 
     model = whisper.load_model("tiny", device="cpu")
 
@@ -14,8 +15,9 @@ def filter_json_by_keyword(json_data, keyword):
     
     only_words_array = map_json_data(json_data)
     filtered_results = []
-    for word in only_words_array: 
-        if keyword in word["text"]:
+    for word in only_words_array:
+        word_text = re.sub('[^A-Za-z0-9]+', '', word["text"]).lower() 
+        if keyword.lower() in word_text:
             filtered_results.append(word)
 
     return filtered_results
