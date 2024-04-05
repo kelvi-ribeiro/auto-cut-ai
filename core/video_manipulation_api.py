07af096f6417999bfc5bd441a4e85f0bdc0ebc21
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2024-04-05 20:09:35 -0300

    Using max number of cpu on writeVideoFile function

--- core/video_manipulation_api.py
+++ core/video_manipulation_api.py
@@ -1,19 +1,21 @@
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.editor import concatenate_videoclips
 import os
+import multiprocessing
 
 def cut_video(video, cuts, seconds_to_cut):
     cut_segments = []
     for cut in cuts:
-        start_time = (cut.get("start") - seconds_to_cut)
-        end_time = cut.get("end")
-        ## TODO Tratar de forma melhor, se start for maior que início do vídeo, pegar início do vídeo como referência
-        ## TODO Se o final do vídeo for maior que a duração, pegar tempo de vuração como tempo final
-        if 0 <= start_time <= video.duration and 0 <= end_time <= video.duration:
-            segment = video.subclip(start_time, end_time)
-            cut_segments.append(segment)
-        else:
-            print(f"Ignoring invalid cut: {cut}")
+        start_time = (cut.get("end") - seconds_to_cut)
+        end_time = cut.get("end") + 1
+        if start_time < 0: 
+            start_time = 0
+        if end_time > video.duration:
+            end_time = video.duration
+            
+        segment = video.subclip(start_time, end_time)
+        cut_segments.append(segment)
+
     return cut_segments
 
 def generate_video(video_path, times_of_each_keyword_spoken, seconds_to_cut):
@@ -28,5 +30,5 @@ def generate_video(video_path, times_of_each_keyword_spoken, seconds_to_cut):
     else:
         combined_video = concatenate_videoclips(cut_segments) 
         output_path = "export/" + os.path.basename(video_path) 
-        combined_video.write_videofile(output_path)
+        combined_video.write_videofile(output_path, threads=multiprocessing.cpu_count())
         video.close()
