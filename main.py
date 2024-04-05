83223b10025629fb72ebfacf8450396d4cb237aa
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2024-04-05 20:08:57 -0300

    Printing the processed time in minutes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import core.video_manipulation_api as video_manipulation
 import core.voice_recognition_api as voice_recognition
 import json
-import datetime
+import datetime as dt
 
 def generate_cut_video(video_path, keyword, seconds_to_cut, useDebugFile):
     times_of_each_keyword_spoken = voice_recognition.get_times_of_each_keyword_spoken(keyword, video_path, useDebugFile)
@@ -11,6 +11,10 @@ def generate_cut_video(video_path, keyword, seconds_to_cut, useDebugFile):
 with open('config.json') as config_file:
     config = json.load(config_file)
 
-print(f"Initiating main process in {datetime.datetime.now()}...")
+start_time = dt.datetime.now()
+print(f"Initiating main process at {start_time}...")
 generate_cut_video(config['video_path'], config['keyword'], config['seconds_to_cut'], config['useDebugFile'])
-print(f"Finishing main process in {datetime.datetime.now()}...")
+end_time = dt.datetime.now()
+
+processing_time = (end_time - start_time).total_seconds() / 60
+print(f"Finishing main process at {end_time}. Processing time: {processing_time:.2f} minutes.")
