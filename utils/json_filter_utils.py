7693586da8308df2562e2861f77492adf9b91128
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2024-04-06 01:07:02 -0300

    Parameterizing the confidence level

--- utils/json_filter_utils.py
+++ utils/json_filter_utils.py
@@ -1,5 +1,6 @@
 
 import utils.string_utils as string_utils
+import json
 
 def filter_json_by_keyword(json_data, keyword):
     print(f"About to map the '{keyword}' in result" )
@@ -8,8 +9,10 @@ def filter_json_by_keyword(json_data, keyword):
     
     only_words_array = map_json_data(json_data)
     filtered_results = []
+    with open('config.json') as config_file:
+            config = json.load(config_file)
     for word in only_words_array:
-        if string_utils.remove_special_chars_and_accents(keyword) in string_utils.remove_special_chars_and_accents(word["text"]):
+        if string_utils.remove_special_chars_and_accents(keyword) in string_utils.remove_special_chars_and_accents(word["text"]) and word["confidence"] > config["minimum_confidence"]:
             print(f"{word}' found in array")
             filtered_results.append(word)
 
